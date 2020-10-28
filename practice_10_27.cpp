fd1355a451697321b4f865337077e96453a585c3
juruo-c <2915601267@qq.com>
2020-10-28 18:51:39 +0800

    日常练习

---
+++ practice_10_27.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//#include<cstdio>
+//#include<cstring>
+//using namespace std;
+//int main()
+//{
+//	int alp[26] = { 1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,4,1,2,3,1,2,3,4 };
+//	char a[201];
+//	gets(a);
+//	int len = strlen(a);
+//	int cnt = 0;
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (a[i] == ' ')
+//			cnt++;
+//		else
+//			cnt += alp[a[i] - 'a'];
+//	}
+//	cout << cnt;
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	char s[101];
+	cin >> s;
+
+	return 0;
+}
