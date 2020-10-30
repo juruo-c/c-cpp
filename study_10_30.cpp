6d42b577217f07c92b7b1ea1a1e7995369e8a60b
juruo-c <2915601267@qq.com>
2020-10-30 23:16:57 +0800

    ACM入门学习录01

---
+++ study_10_30.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+//int main()
+//{
+//	printf("%d", 111111111 * 111111111);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%f", 111111111.0*111111111);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%f\n", sqrt(-10));
+//	printf("%d", sqrt(-10));
+//	printf("%lf\n", sqrt(-10));
+//	return 0;
+//}
