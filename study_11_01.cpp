3dd96bde733fd02ed4536bba7231674d7c1c6f83
juruo-c <2915601267@qq.com>
2020-11-01 23:21:53 +0800

    算法入门学习录03

---
+++ study_11_01.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	int i;
+//	for (i = 10; i <= 100; i++)
+//	{
+//		if (i % 3 == a && i % 5 == b && i % 7 == c)
+//		{
+//			printf("%d", i);
+//			break;
+//		}
+//	}
+//	if (i > 100)
+//		printf("No answer");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	int i, j;
+//	int k;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (k = 0; k < i; k++)
+//			printf(" ");
+//		for (j = 0; j < 1 + (n - i - 1) * 2; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
